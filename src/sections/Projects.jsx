e9ff0122f9b8c78dd5c16b4e56880fe01a1670c0
Belen Almendros <belen.almendros19@gmail.com>
2026-04-20 18:11:48 -0400

    feat(projets): add images, styles and animations

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -2,34 +2,34 @@ const Projects = () => {
   const projects = [
     {
       name: "Escuelita Libre",
-      image: "https://raw.githubusercontent.com/BelAlmendras/portafolio/main/escuelita-libre.png",
+      image: "../public/escuelita-libre.png",
       description:
         "Desarrollé la interfaz de un sitio educativo enfocada en una experiencia accesible y atractiva para nuevos apoderados y estudiantes. Implementado con React, Tailwind y Vite, prioricé un diseño limpio, intuitivo y orientado a la conversión.",
-      github: "#",
+      github: "https://github.com/BelAlmendras/escuelita-libre-ui",
       demo: "#",
     },
     {
       name: "Joyeria Hoseki",
-      image: "https://raw.githubusercontent.com/BelAlmendras/portafolio/main/hoseki.png",
+      image: "../public/hoseki.jpg",
       description:
         "Proyecto fullstack desarrollado en equipo bajo metodología ágil (Scrum). Participé en la construcción del frontend con React y en la integración con backend en Spring Boot y base de datos PostgreSQL, colaborando en la implementación de un flujo completo de e-commerce.",
-      github: "#",
-      demo: "#",
+      github: "https://github.com/BelAlmendras/SA-HA/",
+      demo: "https://hoseki.vercel.app/",
     },
     {
       name: "¿Quién es ese Pokémon?",
-      image: "https://raw.githubusercontent.com/BelAlmendras/portafolio/main/pokemon.png",
+      image: "../public/quien-es-ese-pokemon.png",
       description:
         "Minijuego inspirado en “¿Quién es ese Pokémon?” que consume la API de Pokémon. Implementé la ocultación y revelado de sprites mediante interacción del usuario, junto con efectos visuales para generar una experiencia lúdica y dinámica",
-      github: "#",
-      demo: "#",
+      github: "https://github.com/BelAlmendras/quien-es-ese-pokemon/",
+      demo: "https://quien-es-ese-pokemon-nine.vercel.app/",
     },
   ];
 
-  return (
-  <section id="projects" className="text-slate-100 py-20 px-6">
+return (
+  <section id="projects" className="text-slate-100 py-20 px-6 ">
     <div className="max-w-6xl mx-auto">
-      <h2 className="text-3xl md:text-4xl font-bold text-indigo-400 text-center mb-12">
+      <h2 className="text-3xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-400 via-purple-400 to-indigo-400 text-center mb-12">
         Proyectos
       </h2>
 
@@ -37,34 +37,46 @@ const Projects = () => {
         {projects.map((project) => (
           <div
             key={project.name}
-            className="bg-slate-800 rounded-xl p-6 shadow-lg hover:scale-105 hover:bg-slate-700 transition flex flex-col justify-between"
+            className="relative bg-gradient-to-br from-slate-800/80 to-indigo-900/60 backdrop-blur-md rounded-xl p-6 shadow-lg border border-white/10 hover:scale-105 hover:shadow-purple-500/20 transition flex flex-col justify-between overflow-hidden"
           >
+            
+            <div className="absolute -top-10 -right-10 w-32 h-32 bg-pink-500/20 rounded-full blur-2xl"></div>
+            <div className="absolute -bottom-10 -left-10 w-32 h-32 bg-indigo-500/20 rounded-full blur-2xl"></div>
+
             <div>
-          
               <div className="mb-4">
                 <img
                   src={project.image}
                   alt={project.name}
-                  className="w-full h-32 object-cover rounded-lg"
+                  className="w-full h-32 object-cover rounded-lg border border-white/10"
                 />
               </div>
 
               <h3 className="text-xl font-semibold mb-3">{project.name}</h3>
 
-              <p className="text-slate-300 mb-6">{project.description}</p>
+             <p className="text-slate-300 mb-6">{project.description}</p>
             </div>
 
             <div className="flex gap-4">
               <a
                 href={project.github}
-                className="px-4 py-2 bg-gray-700 rounded-lg hover:bg-gray-600 transition"
+                className="flex items-center justify-center gap-2 
+              px-5 py-3 rounded-xl 
+              bg-gradient-to-r from-purple-600 to-fuchsia-600 
+              text-white! font-medium
+              md:hover:scale-105 transition-all"
               >
                 GitHub
               </a>
 
               <a
                 href={project.demo}
-                className="px-4 py-2 bg-indigo-500 rounded-lg hover:bg-indigo-600 transition"
+className="flex items-center justify-center gap-2 
+              px-5 py-3 rounded-xl 
+              bg-slate-800/60 backdrop-blur-md 
+              border border-white/10 text-white!
+              md:hover:bg-slate-700/60 md:hover:scale-105
+              transition-all"
               >
                 Demo
               </a>
